395df6c979837ead67bbe8b2ff4c5b70d5bc56b6
Nicolás Córdoba <58666053+nicoargg@users.noreply.github.com>
2022-04-19 16:40:42 -0300

    Consulta SQL universidades grupo H (#5)

---
+++ airflow/include/SQL_Universidad_del_Cine.sql
@@ -0,0 +1,16 @@
+SELECT 
+	universities AS university,
+	careers AS career,
+	inscription_dates AS inscription_date,
+	SPLIT_PART(names, '-', 1) first_name,
+	SPLIT_PART(names,'-',2) last_name,
+	sexo AS gender,
+	birth_dates AS birth_date,
+	direccion AS location,
+	emails AS email
+FROM 
+	public.lat_sociales_cine
+where
+	universities = 'UNIVERSIDAD-DEL-CINE'
+	AND TO_DATE(inscription_dates ,'DD-MM-YY')>= '01-09-2020'
+	AND TO_DATE(inscription_dates ,'DD-MM-YY')< '01-02-2021'
