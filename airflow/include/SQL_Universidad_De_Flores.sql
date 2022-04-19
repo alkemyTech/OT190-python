02d215f272d3b61349d886fa8c5c84006e350b32
tmutn <tm.martineztomas@gmail.com>
2022-04-19 13:09:35 -0300

    OT190-12. Consultas SQL para el grupo de universidades A

---
+++ airflow/include/SQL_Universidad_De_Flores.sql
@@ -0,0 +1,13 @@
+SELECT
+	universidad AS university,
+	carrera AS career,
+	TO_DATE(fecha_de_inscripcion, 'YYYY-MM-DD') AS inscription_date,
+	name AS first_name,
+	'' last_name,
+	sexo AS gender,
+	ABS(EXTRACT(YEAR FROM AGE(TO_DATE(fecha_nacimiento, 'YYYY-MM-DD'), NOW()))) AS age,
+	codigo_postal AS postal_code,
+	'' AS location,
+	correo_electronico AS email 	
+FROM
+	flores_comahue
