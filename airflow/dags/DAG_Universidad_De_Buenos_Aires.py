608ee4c609831f28cf7c8bc096aad4cf4a8da962
Nicolas Cordoba <nico343@outlook.es>
2022-04-22 14:26:50 -0300

    bot errors fixed

--- airflow/dags/DAG_Universidad_De_Buenos_Aires.py
+++ airflow/dags/DAG_Universidad_De_Buenos_Aires.py
@@ -1,36 +1,42 @@
-#Python
+# Python
 from datetime import timedelta, datetime
-#Airflow
+# Airflow
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
-# dag sin consultas para universidad del cine
 
-
+# dag sin consultas para universidad de Buenos Aires
 def extract():
-
     pass
+
 
 def transform_data():
-
     pass
 
-def load():
 
+def load():
     pass
 
 
 with DAG(
     "DAG_Universidad_De_Buenos_Aires",
-    description="DAG sin consultas ni procesamiento para la Universidad de Buenos Aires",
+    description="DAG sin procesamiento para la Universidad de Buenos Aires",
     schedule_interval=timedelta(hours=1),
-    start_date=datetime(2022,4,22)
+    start_date=datetime(2022, 4, 22)
 ) as dag:
-    extract_task = PythonOperator(task_id="extract_task", python_callable=extract)
-    
-    transform_data_task = PythonOperator(task_id="transform_data_task", python_callable=transform_data)
+    extract_task = PythonOperator(
+        task_id="extract_task",
+        python_callable=extract
+        )
 
-    load_task = PythonOperator(task_id="load_task", python_callable=load)
-    
+    transform_data_task = PythonOperator(
+        task_id="transform_data_task",
+        python_callable=transform_data
+        )
+
+    load_task = PythonOperator(
+        task_id="load_task",
+        python_callable=load
+        )
 
     extract_task >> transform_data_task >> load_task
