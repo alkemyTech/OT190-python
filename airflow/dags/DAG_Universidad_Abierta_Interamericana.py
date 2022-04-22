22148cddb85afae756359e9488d2d2687abcc3f9
clarisatomasini <clarisatomasini@gmail.com>
2022-04-22 16:02:25 -0300

    OT190-32. Configuración de DAGs para las universidades del grupo E (#15)
    * Configuracion de DAGs para las universidades del grupo E
    
    * Correccion de errores informados por flake8

---
+++ airflow/dags/DAG_Universidad_Abierta_Interamericana.py
@@ -0,0 +1,25 @@
+'''DAG para el procesamiento de datos
+Universidad Abierta Interamericana
+Grupo E'''
+
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+import pendulum
+
+with DAG(
+    'DAG_Universidad_Abierta_Interamericana',
+    start_date=pendulum.datetime(2022, 4, 20, tz="UTC"),
+    schedule_interval='@hourly',
+    catchup=False,
+    tags=["Grupo_Universidades_E"]
+) as dag:
+    # Extraccion de datos a una base de datos postgres
+    # Posibles operadores: PostgresOperator, SqlToS3Operator
+    extract_task = DummyOperator(task_id='extract_task')
+
+    # Procesamiento de datos con pandas y guardar los datos en S3
+    # Posibles operadores: PythonOperator
+    # S3Hook
+    transform_task = DummyOperator(task_id='transform_task')
+
+    extract_task >> transform_task
