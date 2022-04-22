d7b96c099efc6b195c2c7fdcf55b28d625b48082
daianalonso <daalonso@dc.uba.ar>
2022-04-21 22:56:34 -0300

    Configuración inicial del DAG para el grupo de universidades C
    Jira: https://alkemy-labs.atlassian.net/browse/OT190-30?atlOrigin=eyJpIjoiZjljYjI0YWNmM2EwNDZiNDk1NTZhZjEzZWI2NThkYjkiLCJwIjoiaiJ9

---
+++ airflow/dags/DAG_Universidad_de_Palermo.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+
+'''
+Configuración del DAG sin consultas ni procesamiento para la Universidad de Palermo
+'''
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+}
+
+with DAG(
+    'DAG_Universidad_de_Palermo',
+    description='DAG sin consultas ni procesamiento para la Universidad de Palermo',
+    schedule_interval='@hourly', #Que se ejecute cada hora
+    start_date=datetime.now() 
+) as dag:
+    #Solo declaro las tareas de extraer datos, transformarlos y subirlos
+    extract_task = DummyOperator( 
+        task_id = 'extract_task', 
+        dag = dag)
+
+    transform_task = DummyOperator(
+        task_id = 'transform_task',
+        dag = dag)
+    
+    load_task = DummyOperator(
+        task_id = 'load_task',
+        dag = dag)
+
+    # Describo el orden de ejecución en el DAG
+    extract_task >> transform_task >> load_task
