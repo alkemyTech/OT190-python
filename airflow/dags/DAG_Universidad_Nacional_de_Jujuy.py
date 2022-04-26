9d40bf7f2934f27ed6a33600711b108bba9be087
daianalonso <daalonso@dc.uba.ar>
2022-04-26 14:27:11 -0300

    OT190-46: Configurar los log para el grupo de universidades C
    Tarea: https://alkemy-labs.atlassian.net/browse/OT190-46

--- airflow/dags/DAG_Universidad_Nacional_de_Jujuy.py
+++ airflow/dags/DAG_Universidad_Nacional_de_Jujuy.py
@@ -1,11 +1,23 @@
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
+import logging
 
 """
 Configuración del DAG sin consultas ni procesamiento
 para la Universidad Nacional de Jujuy
 """
+
+# Configuracion logging
+# Formato: %Y-%m-%d - nombre_logger - mensaje
+def logging_config():
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(message)s',
+                        level=logging.DEBUG,
+                        datefmt='%Y-%m-%d')
+    log = logging.getLogger(__name__)
+    log.info('Inicializando DAG')
+
 
 default_args = {
     "owner": "airflow",
@@ -20,6 +32,8 @@ with DAG(
     schedule_interval="@hourly",  # Que se ejecute cada hora
     start_date=datetime(2022, 4, 22)
 ) as dag:
+    logging_task= PythonOperator(task_id= "logging", python_callable=logging_config)
+    
     # Solo declaro las tareas de extraer datos, transformarlos y subirlos
     extract_task = DummyOperator(task_id="extract_task", dag=dag)
 
@@ -28,4 +42,4 @@ with DAG(
     load_task = DummyOperator(task_id="load_task", dag=dag)
 
     # Describo el orden de ejecución en el DAG
-    extract_task >> transform_task >> load_task
+    logging_task >> extract_task >> transform_task >> load_task
