ff2a0a9059ba848c3547807c52f21f2278365e24
TanniaLucia <75077280+TanniaLucia@users.noreply.github.com>
2022-04-27 15:12:04 -0500

    Configuración de los log para el grupo de universidades G (#33)

--- airflow/dags/DAG_Universidad_J_F_Kennedy.py
+++ airflow/dags/DAG_Universidad_J_F_Kennedy.py
@@ -1,8 +1,19 @@
-#Configuración de los retries para la Universidad J.F Kennedy
-
+#Configurar los logs para la Universidad J.F Kennedy
+import logging
 from airflow import DAG   
 from datetime import timedelta, datetime 
 from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(message)s',
+    datefmt='%Y-%m-%d',
+    level=logging.DEBUG,
+    )
+logger = logging.getLogger(__name__)
+
+def logging_dags():
+    logger.info('Initializing DAG')
 
 
 #Default settings applied to all tasks
@@ -20,20 +31,20 @@ with DAG(
 ) as dag:
     #Taks to be executed within the DAG
 
+    #Initializing dags
+    logging_dag = PythonOperator(task_id='logging_dag', python_callable=logging_dags)
+
     #Extract data SQL query to Postgres database
     #It is proposed to use PostgresOperator
-
     extract_data = DummyOperator(task_id='extract_data')
-
 
     #Process data with pandas
     #It is proposed to use PythonOperator
     process_data= DummyOperator(task_id='process_data')
-
 
     #Load data to S3
     #It is proposed to use PythonOperator
     load_data= DummyOperator(task_id='load_data')
 
     #Task order
-    extract_data >> process_data >> load_data
+    logging_dag >> extract_data >> process_data >> load_data
