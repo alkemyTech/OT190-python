7103551f0bf4811012e451e40bbcce5ba68b9a19
Dani Lopez Nicolas <lond911@gmail.com>
2022-04-27 10:36:29 -0700

    OT190-37 Configuracion retries grupo de universidades B

--- airflow/dags/DAG_Universidad_Del_Salvador.py
+++ airflow/dags/DAG_Universidad_Del_Salvador.py
@@ -1,6 +1,10 @@
 '''
 DAG para la Universidad Del Salvador, sin consultas, ni procesamiento
 Sprint 2, OT190-29
+
+1.- ETL
+2.- retries
+
 '''
 
 from unittest import FunctionTestCase
@@ -9,25 +13,31 @@ from datetime import timedelta, datetime
 
 from airflow.operators.dummy import DummyOperator
 
+default_args = {
+    'retries': 5,
+    'retry_delay': timedelta(seconds=45)
+}
 
 with DAG(
-    'DAG_Universidad_Del_Salvador',
-    description= 'OT190-29 DAG_Universidad_Del_Salvador, sin consultas, ni procesamiento',
+    'DAG_Universidad_Del_Salvador_ETL_Retries',
+    description= 'OT190-29 DAG_Universidad_Del_Salvador, sin consultas, ni procesamiento,ETL-Retries',
     schedule_interval=timedelta(hours=1),
     start_date= datetime(2024,4,21),
+    default_args=default_args
 ) as dag:
 
     #ETL (Extract, Transform, Load)
-    
+
     # extract_task = PostgresOperator(
     #     task_id='PostgresOperator',
     #     sql='<path>/SQL_Universidad_Del_Salvador.sql',
     #     postgres_conn_id='my_postgres_connection',
     #     autocommit=False
     # )
-    
+
     extract_task = DummyOperator (task_id='extract_task')
     transform_task = DummyOperator(task_id='transform_task')
+    load_task = DummyOperator(task_id='load_task')
 
 
-    extract_task >> transform_task
+    extract_task >> transform_task >> load_task
--- airflow/dags/DAG_Universidad_Nacional_Del_Comahue.py
+++ airflow/dags/DAG_Universidad_Nacional_Del_Comahue.py
@@ -1,6 +1,9 @@
 '''
 DAG para la Universidad_Nacional_Del_Comahue, sin consultas, ni procesamiento
 Sprint 2, OT190-29
+
+1.- ETL
+2.- retries
 '''
 
 from unittest import FunctionTestCase
@@ -10,6 +13,7 @@ from datetime import timedelta, datetime
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
 
+
 def extract():
     # extract_task = PostgresOperator(
     #     task_id='PostgresOperator',
@@ -17,23 +21,30 @@ def extract():
     #     postgres_conn_id='my_postgres_connection',
     #     autocommit=False
     # )
-    
+
     pass
 
 def transform():
     pass
 
+def load():
+    pass
 
+default_args = {
+    'retries': 5,
+    'retry_delay': timedelta(seconds=45)
+}
 
 with DAG(
-    'DAG_Universidad_Nacional_Del_Comahue',
-    description= 'OT190-29 DAG_Universidad_Nacional_Del_Comahue, sin consultas, ni procesamiento',
+    'DAG_Universidad_Nacional_Del_Comahue_ETL_Retries',
+    description= 'OT190-29 DAG_Universidad_Nacional_Del_Comahue, sin consultas, ni procesamiento, ETL-Retries',
     schedule_interval=timedelta(hours=1),
     start_date= datetime(2024,4,21),
+    default_args=default_args
 ) as dag:
 
      #ETL (Extract, Transform, Load)
-    
+
     extract_task = PythonOperator (
         task_id='extract',
         python_callable=extract
@@ -43,6 +54,11 @@ with DAG(
         task_id='transform',
         python_callable=transform
         )
+    
+    load_task = PythonOperator(
+        task_id='load',
+        python_callable=load
+        )
 
 
-    extract_task >> transform_task
+    extract_task >> transform_task >> load_task
