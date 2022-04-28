31e2a96e1ed703ad566a62992bb6a4cf3a0b86b4
tmutn <tm.martineztomas@gmail.com>
2022-04-28 13:10:51 -0300

    Mejoras al código de los dags de OT190-12

--- airflow/dags/DAG_Universidad_Nacional_de_Villa_Maria.py
+++ airflow/dags/DAG_Universidad_Nacional_de_Villa_Maria.py
@@ -3,35 +3,45 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.operators.dummy import DummyOperator
+import pathlib
 
+#Obtener path
+p_path = (pathlib.Path(__file__).parent.absolute()).parent
+
+#Argumentos predeterminados para configuracion
 default_args = {
     'owner': 'airflow',    
     'retries': 5,
     'retry_delay': timedelta(minutes=5),
 }
 
+#Configurar DAG
 with DAG(
-    dag_id = "DAG_Universidad_Nacional_de_Villa_Maria",
+    dag_id="DAG_Universidad_Nacional_de_Villa_Maria",
     default_args=default_args,
     schedule_interval='@once',	
     dagrun_timeout=timedelta(minutes=60),
-    description='ETL para Universidad Nacional de Villa Maria',
+    description='DAG para Universidad Nacional de Villa Maria',
     start_date = datetime(2022,4,25),
-    template_searchpath= '/home/wotw/airflow/includes/',
+    template_searchpath= f'{p_path}/includes',
     catchup=False) as dag:
 
+        #Consultar base de datos
         query_database = PostgresOperator(
             task_id = "query_database",
             sql = 'SQL_Universidad_Nacional_De_Villa_Maria.sql',
             postgres_conn_id = "psql_rds_alkemy_universidades"            
         )
 
+        #Placeholder transformar datos
         pandas_transform = DummyOperator(
             task_id = "pandas_transform"
         )
 
+        #Placeholder cargar datos
         load_to_s3 = DummyOperator(
             task_id = "load_to_s3"
         )
 
+        #Flujo de ejecución
         query_database >> pandas_transform >> load_to_s3
