c2af039b84f448b356112316bac7d6889940668b
Daiana Alonso <daalonso@dc.uba.ar>
2022-05-02 13:03:56 -0300

    OT190-78: Crear una función Python con Pandas para cada universidades C (#42)
    * OT190-78: Crear una función Python con Pandas para cada universidad para el grupo de universidades C
    
    Tarea en Jira: https://alkemy-labs.atlassian.net/browse/OT190-78
    
    * Create codigos_postales.csv
    
    * PEP8
    
    Co-authored-by: Fernando Palacios <fernandogabrielpalacios@gmail.com>

--- airflow/dags/DAG_Universidad_de_Palermo.py
+++ airflow/dags/DAG_Universidad_de_Palermo.py
@@ -33,10 +33,11 @@ def query_to_csv(sql_file, filename):
     try:
         os.stat(path_d)
     except:
-        log.debug('Creando directorio files')
+        log.debug("Creando directorio files")
         os.mkdir(path_d)
 
-    log.debug('Conectando con la base de datos')
+    log.debug("Conectando con la base de datos")
+
     pg_hook = PostgresHook(
         postgres_conn_id="db_alkemy_universidades", schema="training"
     )
@@ -44,17 +45,99 @@ def query_to_csv(sql_file, filename):
     try:
         university_sql = open(f"{path_p}/include/{sql_file}", "r").read()
     except Exception as e:
-        log.error(f'Error: {e}')
-    
-    log.debug('Ejecutando consulta sql y obteniendo resultado')
+        log.error(f"Error: {e}")
+
+    log.debug("Ejecutando consulta sql y obteniendo resultado")
     pandas_df = pg_hook.get_pandas_df(sql=university_sql)
 
-    log.debug(f'Guardando resultado como {filename}')
+    log.debug(f"Guardando resultado como {filename}")
     pandas_df.to_csv(f"{path_d}/{filename}")
 
 
-def normalize_data():
-    pass
+def normalize_strings(columna):
+    """
+    Dada una columna, normaliza los strings a
+    minúsculas, sin espacios extras, ni guiones
+    """
+    try:
+        columna = columna.apply(lambda x: str(x).strip())
+        columna = columna.apply(lambda x: str(x).replace("-", " "))
+        columna = columna.apply(lambda x: str(x).replace("_", " "))
+        columna = columna.apply(lambda x: x.lower())
+    except:
+        log.error("Error al querer normalizar los caracteres")
+    return columna
+
+
+def normalize_data(csv_filename):
+    """
+    Normaliza los datos del .csv pasado por parámetro y los guarda en un .txt
+    """
+    log.info("Leyendo datos del csv")
+    df_univ = pd.read_csv(f"{path_d}/{csv_filename}.csv")
+
+    log.info("Normalizando datos")
+    # university: str minúsculas, sin espacios extras, ni guiones
+    df_univ["university"] = normalize_strings(df_univ["universidad"])
+
+    # career: str minúsculas, sin espacios extras, ni guiones
+    df_univ["career"] = normalize_strings(df_univ["careers"])
+
+    # inscription_date: str %Y-%m-%d format
+    old_date = pd.to_datetime(df_univ["fecha_de_inscripcion"])
+    df_univ["inscription_date"] = pd.to_datetime(old_date, "%Y-%m-%d")
+
+    # first_name: str minúscula y sin espacios, ni guiones
+    # last_name: str minúscula y sin espacios, ni guiones
+    df_univ["names"] = normalize_strings(df_univ["names"])
+    try:
+        df_univ["first_name"] = df_univ["names"].apply(lambda x: str(x).split(" ")[0])
+        df_univ["last_name"] = df_univ["names"].apply(lambda x: str(x).split(" ")[1])
+    except:
+        df_univ["first_name"] = "NULL"
+        df_univ["last_name"] = df_univ["names"]
+
+    # gender: str choice(male, female)
+    df_univ["sexo"] = normalize_strings(df_univ["sexo"])
+    dict_gender = {"f": "female", "m": "male"}
+    df_univ["gender"] = df_univ["sexo"].map(dict_gender)
+
+    # age: int
+    today = datetime.now()
+    df_univ["age"] = df_univ["birth_dates"].apply(
+        lambda x: (100 + (int(str(today.year)[2:4]) - int(x[7:9])))
+    )
+
+    # postal_code: str
+    df_univ["postal_code"] = df_univ["codigo_postal"].astype(str)
+
+    # location: str minúscula sin espacios extras, ni guiones
+    df_cp = pd.read_csv(f"{path_p}/assets/codigos_postales.csv")
+    df_cp["localidad"] = df_cp["localidad"].apply(lambda x: x.lower())
+    dict_cp = dict(zip(df_cp["codigo_postal"], df_cp["localidad"]))
+    df_univ["location"] = df_univ["postal_code"].apply(lambda x: dict_cp[int(x)])
+
+    # email: str minúsculas, sin espacios extras, ni guiones
+    df_univ["email"] = normalize_strings(df_univ["correos_electronicos"])
+
+    # Guardando información necesaria en un .txt
+    df_univ = df_univ[
+        [
+            "university",
+            "career",
+            "inscription_date",
+            "first_name",
+            "last_name",
+            "gender",
+            "age",
+            "postal_code",
+            "location",
+            "email",
+        ]
+    ]
+    log.info(f"Guardando archivo transformado en: datasets/{csv_filename}.txt")
+    df_univ.to_csv(f"{path_p}/datasets/{csv_filename}.txt", sep="\t")
+    return df_univ
 
 
 default_args = {"owner": "airflow", "retries": 5, "retry_delay": timedelta(seconds=30)}
@@ -66,7 +149,7 @@ with DAG(
     schedule_interval="@hourly",  # Que se ejecute cada hora
     start_date=datetime(2022, 4, 22),
     template_searchpath=f"{path_p}/include",
-    catchup=False
+    catchup=False,
 ) as dag:
 
     # Declaro las tareas de extraer datos, transformarlos y subirlos
@@ -77,15 +160,17 @@ with DAG(
             "sql_file": "SQL_Universidad_de_Palermo.sql",
             "filename": "universidad_de_palermo.csv",
         },
-        dag=dag
+        dag=dag,
     )
 
     transform_task = PythonOperator(
-        task_id="transform_task", python_callable=normalize_data, dag=dag
+        task_id="transform_task",
+        python_callable=normalize_data,
+        dag=dag,
+        op_kwargs={"csv_filename": "universidad_de_palermo"},
     )
 
     load_task = DummyOperator(task_id="load_task", dag=dag)
 
     # Describo el orden de ejecución en el DAG
     extract_task >> transform_task >> load_task
-
