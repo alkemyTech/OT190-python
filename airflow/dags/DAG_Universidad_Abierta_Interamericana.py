dcd7295dbbc43aa1754d1b69f3ddecc68eaf04f7
clarisatomasini <clarisatomasini@gmail.com>
2022-05-02 23:43:57 -0300

    Se configura operador para la carga de datos en S3

--- airflow/dags/DAG_Universidad_Abierta_Interamericana.py
+++ airflow/dags/DAG_Universidad_Abierta_Interamericana.py
@@ -10,9 +10,9 @@ import pendulum
 
 from airflow import DAG
 from airflow.models import BaseOperator
-from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.amazon.aws.transfers.local_to_s3 import LocalFilesystemToS3Operator
 from airflow.utils.decorators import apply_defaults
 
 parent_path = (pathlib.Path(__file__).parent.absolute()).parent
@@ -175,10 +175,12 @@ with DAG(
     )
 
     # Carga de datos en S3
-    # Posibles operadores: PythonOperator, LocalFilesystemToS3Operator
-    # S3Hook
-    load_task = DummyOperator(
+    load_task = LocalFilesystemToS3Operator(
         task_id='load_task',
+        filename=f'{parent_path}/datasets/universidad_nacional_de_la_pampa.txt',
+        dest_key='aws_s3_alkemy_universidades',
+        dest_bucket='cohorte-abril-98a56bb4',
+        replace=True,
     )
 
     loginit_task >> extract_task >> transform_task >> load_task
