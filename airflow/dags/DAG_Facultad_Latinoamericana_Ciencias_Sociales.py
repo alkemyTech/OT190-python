716d37cf1e955b61b63749c616ab0924a02b0292
TanniaLucia <ta97lu@gmail.com>
2022-05-03 20:15:11 -0500

    Crear una función Python con Pandas para cada universidad para el grupo de universidades G

--- airflow/dags/DAG_Facultad_Latinoamericana_Ciencias_Sociales.py
+++ airflow/dags/DAG_Facultad_Latinoamericana_Ciencias_Sociales.py
@@ -1,10 +1,12 @@
-
-#Implementar el Python Operator para la Facultad Latinoamericana de Ciencias Sociales
+#Crear una función Python con Pandas para la Facultad Latinoamericana de Ciencias Sociales
 import os
 import logging
 import csv
 from pathlib import Path
 from airflow import DAG   
+import pandas as pd
+from dateutil.relativedelta import relativedelta
+from dateutil import parser
 from datetime import timedelta, datetime 
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
@@ -23,9 +25,9 @@ def logging_dags():
 
 #Obtain path (Current file location)
 parent_path = (Path(__file__).parent.absolute()).parent
+file_university = 'Facultad_Latinoamericana_Ciencias_Sociales'
 
-def extract_data_sql():
-    file_university = 'Facultad_Latinoamericana_Ciencias_Sociales'
+def extract_data_sql(file_university):
     csv_file = f"{parent_path}/files/{file_university}.csv"
 
     #Open sql request
@@ -51,10 +53,48 @@ def extract_data_sql():
             writer.writerow([source[0], source[1], source[2], source[3], source[4], source[5], source[6], source[7]])
 
 
-
-def transform_data():
+def transform_data(file_university):
+    csv_file = f"{parent_path}/files/{file_university}.csv"
+    txt_file = f"{parent_path}/datasets/{file_university}.txt"
+    
+    abreviations = {'mr.': '','dr.': '','mrs.': '','ms.': '','md': '','dds': '','jr.': '','dvm': '','phd': ''}
+    dates = []
+    birth = []
     #Process data obtained from SQL query
-    pass
+    pandas_data = pd.read_csv(csv_file, index_col=False)
+    pandas_data['university'] = (pandas_data['university'].str.replace("-", " ")).str.lower().str.strip()
+    pandas_data['career'] = (pandas_data['career'].str.replace("-", " ")).str.lower()
+    pandas_data['last_name'] = (pandas_data['last_name'].str.replace("-", " ")).str.lower()
+    pandas_data['email'] = (pandas_data['email'].str.replace("-", " ")).str.lower()
+    pandas_data['location'] = (pandas_data['location'].str.replace("-", " ")).str.lower()
+    
+    for i in range(pandas_data.shape[0]):
+        dates.append(parser.parse(pandas_data.loc[i]['inscription_date']).date())
+        birth.append(parser.parse(pandas_data.loc[i]['age']).date())
+    pandas_data['inscription_date'] = pd.to_datetime(dates, format='%Y-%m-%d')
+    pandas_data['age'] = pd.to_datetime(birth, format='%Y-%m-%d')
+    pandas_data['age'] = pandas_data['age'].apply(lambda x: relativedelta(datetime.now(),x).years)
+    
+    # Delete abreviations
+    for abreviation, blank in abreviations.items():
+        pandas_data['last_name'] = pandas_data['last_name'].apply(lambda x: x.replace(abreviation, blank))
+    
+    #Columns are now without trailing and leading spaces
+    pandas_data.columns = pandas_data.columns.str.strip()
+    
+    pandas_data["first_name"] = pandas_data["last_name"].str.split(" ", expand=True)[0]
+    pandas_data["last_name"] = pandas_data["last_name"].str.split(" ", expand=True)[1]
+    pandas_data['gender'] = (pandas_data['gender'].str.replace("M", "male")).str.replace("F","female")
+    
+    #Create folder FILES
+    os.makedirs(f"{parent_path}/datasets", exist_ok=True)
+
+    #Order Columns
+    pandas_data = pandas_data[['university', 'career', 'inscription_date','first_name', 'last_name', 'gender', 'age', 'location', 'email']]
+    
+    #Create file txt
+    pandas_data.to_csv(txt_file, index=None, sep=' ', mode='a')
+
 
 #Default settings applied to all tasks
 default_args = {
@@ -76,15 +116,14 @@ with DAG(
     logging_dag = PythonOperator(task_id='logging_dag', python_callable=logging_dags)
 
     #Extract data SQL query to Postgres database
-
     extract_data = PythonOperator(task_id='extract_data',
-                                  python_callable=extract_data_sql)
-
+                                  python_callable=extract_data_sql,
+                                  op_kwargs={"file_university": file_university})
 
     #Process data with pandas
-    process_data= PythonOperator(task_id='process_data', 
-                                python_callable=transform_data)
-
+    process_data = PythonOperator(task_id='process_data', 
+                                python_callable=transform_data,
+                                op_kwargs={"file_university": file_university})
 
 
     #Load data to S3
