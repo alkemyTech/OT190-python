af65b4186fefc0b502b8b8c6640d06317b1b60f9
TanniaLucia <ta97lu@gmail.com>
2022-05-10 21:55:14 -0500

    Arreglar un Dag dinamico para el grupo de universidades G / Modificados archivos config

--- airflow/dinamic_dags/generator.py
+++ airflow/dinamic_dags/generator.py
@@ -1,17 +1,21 @@
-from jinja2 import Environment, FileSystemLoader
-import yaml
-import pathlib
 import os
+import yaml
+from jinja2 import Environment, FileSystemLoader
 
-path_p = (pathlib.Path(__file__).parent.absolute()).parent
-file_dir = f'{path_p}/templates'
+abspath_file = os.path.abspath(__file__)
+file_dir = os.path.dirname(abspath_file)
+parent_dirname = os.path.split(file_dir)[0]
+
 env = Environment(loader=FileSystemLoader(file_dir))
 template = env.get_template('template_universidades.jinja2')
 
-
 for filename in os.listdir(file_dir):
+
     if filename.endswith(".yaml"):
-        with open(f'{file_dir}/{filename}', "r") as configfile:
+
+        with open(f"{file_dir}/{filename}", "r") as configfile:
             config = yaml.safe_load(configfile)
-            with open(f"dinamic_dags/ETL_{config['dag_id']}.py", "w") as f:
+
+            with open(f"{parent_dirname}/{config['dag_id']}.py", "w") as f:
                 f.write(template.render(config))
+
